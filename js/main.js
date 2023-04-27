3307f72d05dea1f0b417f94485cb20d07914d029
domurso <duom22@gmail.com>
2023-04-27 12:51:08 -0400

    More Pics More Loading More Pain

--- js/main.js
+++ js/main.js
@@ -38,3 +38,56 @@ navLinks.forEach(link => {
     }, 50);
   };
 });
+
+
+imageSet3 = ["img/grad1.jpeg","img/grat2.jpeg","img/grat3.jpeg","img/grat4.jpeg","img/port1.png","img/port2.jpeg","img/port3.jpeg","img/port4.jpeg"];
+imageSet2 = ["img/roxyg1.jpeg","img/roxyg2.jpeg","img/roxyg3.jpeg","img/roxyg4.jpeg","img/jay1.jpeg","img/jay2.jpeg","img/jay3.jpeg","img/jay4.jpeg"];
+imageSet1 = ["img/showcase1.jpeg","img/showcase2.jpeg","img/showcase3.jpeg","img/showcase4.jpeg","img/open1.jpeg","img/open2.jpeg","img/open3.jpeg","img/open4.jpeg"];
+imageSetSet = [imageSet1, imageSet2, imageSet3];
+setIndex = 0;
+const button = document.getElementById('forward');
+button.addEventListener('click', () => {
+  const container = document.getElementById('set1');
+  console.log(container);
+  const images = container.getElementsByTagName('img');
+  console.log(images)
+
+  const containerTwo = document.getElementById('set2');
+  console.log(containerTwo);
+  const imagesTwo = containerTwo.getElementsByTagName('img');
+  console.log(imagesTwo)
+
+  // Replace each image with a new image
+  setIndex+=1;
+  if(setIndex > 2){
+    setIndex = 0;
+  }
+  for (let i = 0; i < images.length; i++) {
+    images[i].src = imageSetSet[setIndex][i];
+    imagesTwo[i].src = imageSetSet[setIndex][i+4];
+  }
+});
+
+const buttonTwo = document.getElementById('backward');
+buttonTwo.addEventListener('click', () => {
+  const container = document.getElementById('set1');
+  console.log(container);
+  const images = container.getElementsByTagName('img');
+  console.log(images)
+
+  const containerTwo = document.getElementById('set2');
+  console.log(containerTwo);
+  const imagesTwo = containerTwo.getElementsByTagName('img');
+  console.log(imagesTwo)
+
+  // Replace each image with a new image
+  setIndex-=1;
+  if(setIndex < 0){
+    setIndex = 2;
+  }
+  for (let i = 0; i < images.length; i++) {
+    images[i].src = imageSetSet[setIndex][i];
+    imagesTwo[i].src = imageSetSet[setIndex][i+4];
+    console.log(imageSetSet[setIndex][i])
+  }
+});
